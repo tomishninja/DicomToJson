d362f93be5ac1afc00778725948674a6dc7f9bb1
tomishninja <tomishninja@gmail.com>
2020-07-20 18:52:31 +0930

    started changes to make this system work with system arguments rather than just the test code

--- DicomToJSON/ConvertDicomToJSON/Program.cs
+++ DicomToJSON/ConvertDicomToJSON/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DicomToJSON;
 using System;
+using System.IO;
 
 namespace ConsoleTest
 {
@@ -13,12 +14,71 @@ namespace ConsoleTest
         
         static void Main(string[] args)
         {
+            // this verible will tell the system were the data is comming from
+            string input = "";
+
+            // this verible will tell the system were to leave the excess data
+            string output = "";
+
+            // this verible will detail the name of the file.
+            string fileName = "Volumetric Data";
+
+
+            // validate the user input
+            if (args.Length == 1)
+            {
+                if (args[0].Trim().ToLower().Equals("help"))
+                {
+                    // help info below
+                }
+                if (Directory.Exists(args[0]))
+                {
+                    input = args[0];
+                    output = args[0];
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid argmuments entered into the command line, The first argmuent must be a valid directory");
+                }
+            }
+            else if (args.Length >= 2)
+            {
+                if (Directory.Exists(args[0]) && Directory.Exists(args[1]))
+                {
+                    input = args[0];
+                    output = args[1];
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid argmuments entered into the command line, The two first argmuent must be a valid directory");
+                }
+            }
+            else if (args.Length == 3)
+            {
+                if (Directory.Exists(args[0]) && Directory.Exists(args[1]))
+                {
+                    input = args[0];
+                    output = args[1];
+                    fileName = args[2];
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid argmuments entered into the command line, The two first argmuent must be a valid directory");
+                }
+            }
+            else
+            {
+                //throw new ArgumentException("No arguments given please input a valid directory to get data from");
+                // if no arguments are given run the test system
+            }
+
+            // Create the object that will load in all of the JSON files
             DicomToJSON.DicomToJson converserionFactory = new DicomToJSON.DicomToJson();
 
+            // store each file in 
+            Console.WriteLine("All Files as JSON will be printed below: ");
 
-            Console.WriteLine("All Files as JSON will be prineted below: ");
-
-            string[] Graphicaldata = converserionFactory.DicomToGraphicalJSONArray(PathToFiles);
+            string[] Graphicaldata = converserionFactory.DicomToGraphicalJSONArray(input);
 
             foreach(string sample in Graphicaldata)
             {
@@ -27,7 +87,9 @@ namespace ConsoleTest
 
             Console.WriteLine("All files printed as a single condensed JSON will be writent below: ");
 
-            Console.WriteLine(converserionFactory.DicomToGraphicalJSON(PathToFiles));
+            Console.WriteLine(converserionFactory.DicomToGraphicalJSON(input));
+
+
 
             Console.ReadKey();
         }
